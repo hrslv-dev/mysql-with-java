02246b7ccbdb8dda712822a5251410570e2241ca
hrslv-dev <henriqueaugustoss17@gmail.com>
2025-09-22 10:47:39 -0300

    commit

--- src/FabricConnection.java
+++ src/FabricConnection.java
@@ -1,6 +1,10 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricConnection {
 
@@ -15,5 +19,12 @@ public class FabricConnection {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+    private static Properties getProperties()throws IOException {
+        Properties prop = new Properties();
+        String path = "conxao.properties";
+
+        prop.load(FabricConnection.class.getResourceAsStream(path));
+
     }
 }
